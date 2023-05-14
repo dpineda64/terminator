6722fc2e4a11fe610ab2f06b8a43bff3ae1d918f
dpineda <daniel64pineda@gmail.com>
2023-05-14 14:16:37 -0600

    update postgrex

--- mix.exs
+++ mix.exs
@@ -34,17 +34,17 @@ defmodule Terminator.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.4"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, "~> 0.15.3"},
+      {:ecto, "~> 3.10"},
+      {:ecto_sql, "~> 3.10"},
+      {:postgrex, "~> 0.17.1"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:optimus, "~> 0.1.0", only: :dev},
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
-      {:ex_machina, "~> 2.2", only: :test},
+      {:ex_machina, "~> 2.7", only: :test},
       {:excoveralls, "~> 0.10", only: :test},
       {:mock, "~> 0.3.0", only: :test},
       {:inch_ex, "2.0.0", only: :docs},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
